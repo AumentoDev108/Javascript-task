7a924b824eb3b48b1d4bf79218a4e5182814e632
Mayank Solanki <aumento.dev108@gmail.com>
2023-06-05 16:45:55 +0530

    Feat (Calculator)

--- Dev/Calculator/assats/script/index.js
+++ Dev/Calculator/assats/script/index.js
@@ -1,171 +1,177 @@
 const showcase = document.getElementById("showcase");
 
 function display(value) {
-  const lastChar = showcase.value.slice(-1);
+    const lastChar = showcase.value.slice(-1);
 
-  if (/\d/.test(value)) {
-    showcase.value += value;
-  } else if (['+', '-', '*', '/', '%'].includes(value)) {
-    if (lastChar.trim() !== '') {
-      showcase.value += ' ' + value + ' ';
-    }
-  } else if (value === '.' && !getLastNumber(showcase.value).includes('.')) {
-    showcase.value += value;
-  } else if (value === '+/-') {
-    const currentValue = showcase.value;
-    const lastNumber = getLastNumber(currentValue);
+    if (/\d/.test(value)) {
+        appendToShowcase(value);
+    } else if (
+        ["+", "-", "*", "/", "%"].includes(value) &&
+        lastChar.trim() !== ""
+    ) {
+        appendToShowcase(` ${value} `);
+    } else if (value === "." && !getLastNumber(showcase.value).includes(".")) {
+        appendToShowcase(value);
+    } else if (value === "+/-") {
+        const currentValue = showcase.value;
+        const lastNumber = getLastNumber(currentValue);
 
-    if (lastNumber !== '') {
-      const toggledValue = parseFloat(lastNumber) * -1;
-      const newExpression = currentValue.slice(0, -lastNumber.length) + toggledValue;
-      showcase.value = newExpression;
+        if (lastNumber !== "") {
+            const toggledValue = parseFloat(lastNumber) * -1;
+            const newExpression =
+                currentValue.slice(0, -lastNumber.length) + toggledValue;
+            showcase.value = newExpression;
+        } else if (currentValue.startsWith("-")) {
+            showcase.value = currentValue.slice(1);
+        } else {
+            showcase.value = `-${currentValue}`;
+        }
+    } else if (value === "CE") {
+        removeLastCharacter();
     }
-  }
 }
 
 function calculate() {
-  const expression = showcase.value;
-  const result = evaluateExpression(expression);
-  showcase.value = result;
+    const expression = showcase.value;
+    const lastChar = expression.slice(-1);
+
+    // Check if the last character is an operator
+    if (["+", "-", "*", "/", "%"].includes(lastChar)) {
+        return;
+    }
+
+    const result = evaluateExpression(expression);
+
+    if (Number.isFinite(result)) {
+        showcase.value = String(result);
+    }
 }
 
-function clearScreen() {      
-  showcase.value = "";
+function clearScreen() {
+    showcase.value = "";
 }
 
 function evaluateExpression(expression) {
-  const tokens = expression.split(' ');
-  let result = parseFloat(tokens[0]);
-  let operator = null;
+    const operators = {
+        "+": (a, b) => a + b,
+        "-": (a, b) => a - b,
+        "*": (a, b) => a * b,
+        "/": (a, b) => a / b,
+        "%": (a, b) => a * (b / 100),
+    };
 
-  for (let i = 1; i < tokens.length; i++) {
-    const token = tokens[i];
+    const tokens = expression.split(" ");
+    const numbers = [];
+    const operatorsStack = [];
 
-    if (['+', '-', '*', '/', '%'].includes(token)) {
-      operator = token;
-    } else {
-      const num = parseFloat(token);
-
-      switch (operator) {
-        case '+':
-          result += num;
-          break;
-        case '-':
-          result -= num;
-          break;
-        case '*':
-          result *= num;
-          break;
-        case '/':
-          result /= num;
-          break;
-        case '%':
-          result %= num;
-          break;
-      }
+    for (const token of tokens) {
+        if (operators.hasOwnProperty(token)) {
+            while (
+                operatorsStack.length > 0 &&
+                operators[token](0, 0) <=
+                operators[operatorsStack[operatorsStack.length - 1]](0, 0)
+            ) {
+                const operator = operatorsStack.pop();
+                const b = numbers.pop();
+                const a = numbers.pop();
+                const result = operators[operator](a, b);
+                numbers.push(result);
+            }
+            operatorsStack.push(token);
+        } else if (!isNaN(parseFloat(token))) {
+            numbers.push(parseFloat(token));
+        }
     }
-  }
 
-  return result;
+    while (operatorsStack.length > 0) {
+        const operator = operatorsStack.pop();
+        const b = numbers.pop();
+        const a = numbers.pop();
+        const result = operators[operator](a, b);
+        numbers.push(result);
+    }
+
+    return numbers.length === 1 ? numbers[0] : NaN;
 }
 
 function getLastNumber(expression) {
-  const tokens = expression.split(' ').reverse();
+    const tokens = expression.split(" ").reverse();
 
-  for (const token of tokens) {
-    if (/^\d/.test(token)) {
-      return token;
+    for (const token of tokens) {
+        if (/^\d/.test(token)) {
+            return token;
+        }
     }
-  }
 
-  return '';
+    return "";
+}
+
+function appendToShowcase(value) {
+    showcase.value += value;
 }
 
 function handleKeyDown(event) {
-  const allowedKeysRegex = /^[\d+\-*/%.]$/;
-  const keyPressed = event.key;
+    const allowedKeysRegex = /^[\d+\-*/%.]$/;
+    const keyPressed = event.key;
 
-  if (keyPressed === 'Backspace') {
-    event.preventDefault();
-    showcase.value = showcase.value.slice(0, -1); // Remove the last character
-  } else if (keyPressed === 'Enter') {
-    event.preventDefault();
-    calculate();
-  } else if (!allowedKeysRegex.test(keyPressed)) {
-    event.preventDefault();
-  } else {
-    if (['+', '-', '*', '/', '%'].includes(keyPressed)) {
-      display(keyPressed);
-      event.preventDefault();
-    } else if (keyPressed === '+/-') {
-      display(keyPressed);
-      event.preventDefault();
-    } else if (keyPressed === '.') {
-      display(keyPressed);
-      event.preventDefault();
+    if (keyPressed === "Backspace") {
+        event.preventDefault();
+        removeLastCharacter();
+    } else if (keyPressed === "Enter") {
+        event.preventDefault();
+        calculate();
+    } else if (
+        allowedKeysRegex.test(keyPressed) &&
+        (["+",
+                "-", "*", "/", "%"
+            ].includes(keyPressed) ||
+            keyPressed === "." ||
+            /\d/.test(keyPressed))
+    ) {
+        display(keyPressed);
+        event.preventDefault();
+    } else {
+        event.preventDefault();
     }
-  }
+}
+
+function removeLastCharacter() {
+    const currentValue = showcase.value;
+    const newValue = currentValue.slice(0, -1);
+    showcase.value = newValue;
+}
+
+function handlePaste(event) {
+    const element = event.target;
+
+    if (element.tagName === "INPUT" && element.type === "text") {
+        const clipboardData = event.clipboardData || window.clipboardData;
+        const pastedText = clipboardData.getData("text/plain").trim();
+
+        if (/^[0-9+\-*/%.]+$/.test(pastedText)) {
+            const currentValue = element.value;
+            const selectionStart = element.selectionStart;
+            const selectionEnd = element.selectionEnd;
+            const newValue =
+                currentValue.substring(0, selectionStart) +
+                pastedText +
+                currentValue.substring(selectionEnd);
+
+            element.value = newValue;
+            element.setSelectionRange(
+                selectionStart + pastedText.length,
+                selectionStart + pastedText.length
+            );
+
+            event.preventDefault();
+        } else {
+            event.preventDefault();
+        }
+    }
 }
 
 showcase.addEventListener("keydown", handleKeyDown);
+showcase.addEventListener("paste", handlePaste);
 document.getElementById("equals-button").addEventListener("click", calculate);
-document.getElementById("clear-button").addEventListener("click", clearScreen); 
-
-
-
-
-
-
-
-
-
-
-
-
-
-// function display(val) {
-//     document.getElementById('result').value += val;
-//     console.log(val);
-// }
-
-// function solve() {
-//     const x = document.getElementById('result').value;
-//     const y = calculate(x);
-//     document.getElementById('result').value = y;
-//     console.log(x);
-// }
-
-// function clearScreen() {
-//     document.getElementById('result').value = '';
-//     console.log(clearScreen);
-// }
-
-// function operator(val) {
-//     document.getElementById('result').value += val;
-//     console.log(operator);
-// }
-
-// function calculate(expression) {
-//     var operands = expression.split(/[\+\-\*\/]/);
-//     var operators = expression.split(/\d+/).filter(Boolean);
-
-//     var result = Number(operands[0]);
-//     for (var i = 0; i < operators.length; i++) {
-//         const operator = operators[i];
-//         const operand = Number(operands[i+1]);  
-//         if (operator === '+') {
-//             result += operand;
-//         } else if (operator === '-') {
-//             result -= operand;
-//         } else if (operator === '*') {
-//             result *= operand;
-//         } else if (operator === '/') {
-//             result /= operand;
-//         }
-//     }
-
-//     return result;
-// }
-
-
-  
+document.getElementById("clear-button").addEventListener("click", clearScreen);
+document.getElementById("ce-button").addEventListener("click", removeLastCharacter);
